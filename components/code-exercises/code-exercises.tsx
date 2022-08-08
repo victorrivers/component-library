31f67690a6b7a8122350e7f85555ee86e1bc27af
Victor <victor_github@outlook.com>
2022-08-08 11:23:28 +0200

    permutation-check

--- components/code-exercises/code-exercises.tsx
+++ components/code-exercises/code-exercises.tsx
@@ -3,7 +3,8 @@ import { useEffect, useRef } from "react";
 //import run from "../../code-exercises/cyclic-rotation/cyclic-rotation";
 //import run from "../../code-exercises/smallest-positive-integer/smallest-positive-integer";
 //import run from "../../code-exercises/frog-jump/frog-jump";
-import run from "../../code-exercises/count-conforming-bitmasks/count-conforming-bitmasks";
+//import run from "../../code-exercises/count-conforming-bitmasks/count-conforming-bitmasks";
+import run from "../../code-exercises/permutation-check/permutation-check";
 
 export function CodeExercises(): JSX.Element {
 	const loaded = useRef(false);
